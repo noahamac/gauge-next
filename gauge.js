0aca8059332acf3b111e9d7dc2c195fb1dfaa17d
noahamac <noah.macdonald@looker.com>
2020-01-13 09:46:56 -0800

    defaults

--- gauge.js
+++ gauge.js
@@ -62,7 +62,7 @@ const visObject = {
 			cutout: .25,	// Size of Gauge Cutout
 			color: '#DB4437',	// Color of Gauge fill
 			arm: 15,					// Arm extension
-			arm_weight: 4,		// Arm weight
+			arm_weight: 8,		// Arm weight
 			gauge_background: '#CECECE',	// Gauge background color
 			spinner_background: '#282828',	// Spinner background color
 			spinner_weight: 6,	// Spinner Weight
